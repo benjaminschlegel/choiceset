a5a8511b0ea951a509d32623c1308994c49570db
benjaminschlegel <kontakt@benjaminschlegel.ch>
2019-03-19 11:51:55 +0100

    fixed estimation

--- R/optimal_cutpoint.R
+++ R/optimal_cutpoint.R
@@ -34,9 +34,9 @@ optimal_cutpoint <- function (data, ..., choice, metric = c("Accuracy", "Kappa",
   max <- max(ptv.matrix)
   choice.dummy <- dummies::dummy(choice)
   include <- array(NA, dim = c(nrow(ptv.matrix), ncol(ptv.matrix),
-                               max - min - 1))
-  for (i in min:(max - 1)) {
-    include[, , i] = ptv.matrix > i
+                               max - min + 1))
+  for (i in min:max) {
+    include[, , i+1] <- ptv.matrix >= i
   }
   storage.mode(include) <- "numeric"
   metric <- match.arg(metric)
@@ -44,22 +44,58 @@ optimal_cutpoint <- function (data, ..., choice, metric = c("Accuracy", "Kappa",
     names <- levels(as.factor(choice))
   }
   perform <- matrix(NA, nrow = max - min - 1, ncol = ncol(ptv.matrix))
+
+  # CODE FOR COMPUTING VARIOUS QUANTITIES
+  t <- c(12,0,8,0)
+  t <- matrix(t, nrow=2, ncol=2, byrow=F)
+  row.names(t) <- c("1", "0")
+  colnames(t) <- c("1", "0")
+
   for (i in seq_len(ncol(ptv.matrix))) {
     for (j in seq_len(max - min - 1)) {
-      t = table(include[, i, j], choice.dummy[, i])
-      if (metric == "Accuracy")
-        perform[j, i] <- caret::confusionMatrix(t, positive = "1")$overall[1]
-      else if (metric == "Kappa")
-        perform[j, i] <- caret::confusionMatrix(t, positive = "1")$overall[2]
-      else if (metric == "Sensitivity")
-        perform[j, i] <- caret::confusionMatrix(t, positive = "1")$byClass[1]
-      else if (metric == "Specificity")
-        perform[j, i] <- caret::confusionMatrix(t, positive = "1")$byClass[2]
-      else perform[j, i] <- caret::confusionMatrix(t, positive = "1")$byClass[11]
+      t <- table(include[, i, j], choice.dummy[, i])
+      if(sum(dim(t) != c(2, 2))>0){ # not 2x2
+        temp <- matrix(0, nrow = 2, ncol = 2)
+        colnames(temp) <- rownames(temp) <- 0:1
+        r <- which(rownames(t) == rownames(temp))
+        c <- which(colnames(t) == colnames(temp))
+        temp[r, c] <- t
+        t <- temp
+      }
+      perform[j, i] <- confusionMatrix(t, metric)
     }
   }
+  return(perform)
   party.cutpoints <- apply(perform, 2, which.max) - min - 1
   global.cutpoint <- which.max(rowMeans(perform)) - min - 1
   names(party.cutpoints) <- names
   list(party.cutpoints = party.cutpoints, global.cutpoint = global.cutpoint)
+}
+
+
+confusionMatrix <- function(table, type, positive = 1) {
+  n <- sum(colSums(table))
+  acc <- (1/n) * (table[1,1]+table[2,2])
+  row.m <- rowSums(table)
+  col.m <- colSums(table)
+  chance.freq <- (row.m[1]*col.m[1])/n + (row.m[2]*col.m[2])/n
+  kappa <- (acc-chance.freq/n)/(1-chance.freq/n)
+  sensitivity <- ifelse(positive == 1, table[1,1]/col.m[1], table[2,2]/col.m[2])
+  specificity <- ifelse(positive == 1, table[2,2]/col.m[2], table[1,1]/col.m[1])
+  balanced <- .5*(sensitivity+specificity)
+  if(type == "Accuracy"){
+    return(acc)
+  }
+  if(type == "Kappa"){
+    return(kappa)
+  }
+  if(type == "Balanced"){
+    return(balanced)
+  }
+  if(type == "Sensitivity"){
+    return(sensitivity)
+  }
+  if(type == "Specificity"){
+    return(specificity)
+  }
 }
